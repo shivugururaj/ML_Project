d18fc287af1bfd748392ec7577dd0351e0a60130
Shivu Gururaj <gururajshivu@gmail.com>
2016-04-24 00:44:23 -0500

    script update

--- python/run.sh
+++ python/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-python csv2libsvm.py ../output/train_users_libsvm.csv ../output/train_users_libsvm.txt 13
-echo "Written lbsvm output to output/train_users_libsvm.txt"
+python csv2libsvm.py /Users/shivugururaj/Documents/problemsolving/mlproject/output/train_users_libsvm.csv /Users/shivugururaj/Documents/problemsolving/mlproject/output/train_users_libsvm.txt 13
+echo "Written train libsvm output to output/train_users_libsvm.txt"
+
+python csv2libsvm.py /Users/shivugururaj/Documents/problemsolving/mlproject/output/test_users_libsvm.csv /Users/shivugururaj/Documents/problemsolving/mlproject/output/test_users_libsvm.txt -1
+echo "Written test libsvm output to output/test_users_libsvm.txt"
